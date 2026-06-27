b3755aee26e804671147e2e080cff468727c25a1
Victor Mendonça <vhugolm@gmail.com>
2026-06-26 21:55:53 -0300

    cap4 primeiro

---
+++ fundamentos/src/main/java/com/mycompany/fundamentos/DesasfioAritimetico.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.fundamentos;
+
+/**
+ *
+ * @author victormendonca
+ */
+public class DesasfioAritimetico {
+    public static void main(String[] args) {
+        
+        double a = (Math.pow((Math.pow((6*(3+2)), 2) / (3*2)) - (Math.pow(((1-5)*(2-7)/2), 2)), 3))/Math.pow(10, 3);
+        System.out.println(a);
+    }
+    
+}
