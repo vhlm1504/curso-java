a0b317962450823d6227ad8cb97bb24475bf20a2
Victor Mendonça <vhugolm@gmail.com>
2026-06-30 16:49:10 -0300

    cap4 primeiro

---
+++ fundamentos/src/main/java/com/mycompany/classes/Produto.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.classes;
+
+/**
+ *
+ * @author victormendonca
+ */
+public class Produto {
+    
+    String nome;
+    double preco; 
+    double desconto;
+    
+}
---
+++ fundamentos/src/main/java/com/mycompany/classes/ProdutoTest.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.classes;
+
+/**
+ *
+ * @author victormendonca
+ */
+public class ProdutoTest {
+    public static void main(String[] args) {
+        
+        int a = 3;
+        
+        Produto p1 = new Produto();
+        p1.nome = "Lapttop";
+        p1.preco = 4356.34;
+        p1.desconto = 0.25;
+        
+        var p2 = new Produto();//instanciando por inferência
+        p2.nome = "Caneta Preta";
+        p2.preco = 12.60;
+        p2.desconto = 0.29;
+        
+        System.out.println(p1.nome);
+        System.out.println(p2.nome);
+        
+        double precoFinal1 = p1.preco * (1 - p1.desconto);
+        double precoFinal2 = p2.preco * (1 - p2.desconto);
+        System.out.println(precoFinal1 + precoFinal2);
+    }
+    
+}
